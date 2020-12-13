8c2dc63c46c94c7bc12f7363af7aa15c39685424
Monikadeshmukh01 <Monikadeshmukh01@gmail.com>
2020-12-13 16:56:16 +0530

    spring api

---
+++ spring-boot-hello-world-example/src/main/java/com/javaproject/springboothelloworldexample/Students.java
@@ -0,0 +1,23 @@
+package com.javaproject.springboothelloworldexample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Students {
+    private List<Student> studentList;
+
+    public List<Student> getStudentList()
+    {
+        if (studentList == null) {
+            studentList = new ArrayList<>();
+        }
+
+        return studentList;
+    }
+    public void setStudentList(List<Student> studentList)
+    {
+        this.studentList = studentList;
+    }
+}
+
+
---
+++ spring-boot-hello-world-example/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into student
+values(10001,'Monika', 'E1234567');
+
+insert into student
+values(10002,'Vedu', 'A1234568');
+
+insert into student
+values(10003,'Swapnil', 'E123123');
+
+insert into student
+values(10004,'rishu', 'A1234555');
